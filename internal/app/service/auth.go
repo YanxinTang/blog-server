aa32a6ebf8afe23b2f87e2a62c2e4bac9b489750
tyx1703 <tyx1703@gmail.com>
2022-07-30 12:16:25 +0800

    fix: 注册成功后禁用注册功能

--- internal/app/service/auth.go
+++ internal/app/service/auth.go
@@ -10,6 +10,10 @@ import (
 func InitUserAndSetting(user ent.User) error {
 	return utils.WithTx(common.Context, common.Client, func(tx *ent.Tx) error {
 		_, err := model.CreateUser(common.Context, tx.Client())(user.Username, user.Email, user.Password)
+		if err != nil {
+			return err
+		}
+		_, err = model.SetSetting(common.Context, tx.Client())("signupEnable", "0")
 		return err
 	})
 }
